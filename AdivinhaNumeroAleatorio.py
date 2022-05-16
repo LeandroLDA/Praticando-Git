8facd93273b5d3f650caf2540a8eeb9c082a8e89
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2022-05-15 22:16:52 -0300

    Commit Inicial

---
+++ AdivinhaNumeroAleatorio.py
@@ -0,0 +1,9 @@
+import random
+x=5#3random.random()'
+y=eval(input("Digite um possivel número para x: "))
+if y==x :
+    print("você acertou!")
+elif y>x :
+    print("Sue número é maior")
+else:
+    print("Seu número é menor")
